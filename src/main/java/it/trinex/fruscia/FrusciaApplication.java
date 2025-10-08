b32e2ceb49bde21b09ed62b4813f56e77f79a6e5
Andrea Gatta <andrew@gat.tax>
2025-10-08 20:02:28 +0200

    First Commit

---
+++ src/main/java/it/trinex/fruscia/FrusciaApplication.java
@@ -0,0 +1,13 @@
+package it.trinex.fruscia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrusciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrusciaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/trinex/fruscia/FrusciaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.trinex.fruscia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrusciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
